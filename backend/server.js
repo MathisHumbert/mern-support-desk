94375b32b7735e7e3cefd20088798e40ce546071
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-12 18:48:44 +0100

    working on the back

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
 require('dotenv').config();
+require('colors');
 
 const express = require('express');
 const app = express();
 
+const connectDB = require('./config/db');
 const userRouter = require('./routes/userRoutes');
+const errorHandler = require('./middleware/errorMiddleware');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Connect to database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
 app.use('/api/users', userRouter);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
