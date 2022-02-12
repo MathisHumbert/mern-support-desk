197808c82dac4848ca573401d05453d4562b6271
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-12 18:13:08 +0100

    working on the back

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('Server...');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const userRouter = require('./routes/userRoutes');
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.use('/api/users', userRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
